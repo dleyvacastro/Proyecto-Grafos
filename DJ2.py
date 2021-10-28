02b00c2877f629a5d62fcbfaaaa306645bbf23f3
dleyvacastro <dleyvacastro@gmail.com>
2021-10-28 18:19:46 -0500

    Dijkstra Completo

--- DJ2.py
+++ DJ2.py
@@ -14,6 +14,12 @@ from igraph import *
 #            positions.append(k)
 #
 #    return positions
+def get_path(L, u, z, r=[]):
+    lz = L[z][1]
+    r.append(lz[-1])
+    if u in r:
+        return r
+    return get_path(L, u, lz[-1], r)
 
 
 def w(G, x, v):
@@ -30,12 +36,12 @@ def Dijkstra(G, u, z):
     #id = G.get_eid("Cowboy Bebop", "One Piece")
     # print(G.es[id]['weight'])
 
-    L = {i: float('inf') for i in G.vs["name"]}
-    L[u] = 0
+    L = {i: [float('inf'), []] for i in G.vs["name"]}
+    L[u] = [0, []]
     S = []
 
     while z not in S:
-        L_S = {i: L[i] for i in L if i not in S}
+        L_S = {i: L[i][0] for i in L if i not in S}
         x = min(L_S, key=L_S.get)
         # for i in minimums(L):
         #    if i not in S:
@@ -45,9 +51,18 @@ def Dijkstra(G, u, z):
         #        break
 
         S = list(set().union(S, [x]))
+        # print(L)
         for v in G.vs["name"]:
+            # print(v)
             if v not in S:
-                L[v] = min(L[v], L[x] + w(G, x, v))
-
-    print(f'{u} -- {z}: {L[z]}')
-    return S, L
+                if L[v][0] < L[x][0] + w(G, x, v):
+                    L[v] = [L[v][0], L[v][1]]
+                else:
+                    L[v][1].append(x)
+                    L[v] = [L[x][0] + w(G, x, v), L[v][1]]
+                # L[v] = min(L[v], L[x] + w(G, x, v))
+    path = get_path(L, u, z, [])
+    path = path[::-1]
+    path.append(z)
+    print(f'{u} -- {z}: {path} w = {L[z][0]}')
+    return L
