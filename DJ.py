a1fc2122ba00da0d08c489d140f0dbc250c5a005
dleyvacastro <dleyvacastro@gmail.com>
2021-10-31 11:48:02 -0500

    remplazo del Dijstra

--- DJ.py
+++ DJ.py
@@ -1,40 +1,70 @@
-def DJ(u , z, A, B, C):
-    L = []
+from igraph import *
+
+
+# def minimums(some_dict):
+#    # Tomada de Stackoverflow
+#    positions = []  # output variable
+#    min_value = float("inf")
+#    for k, v in some_dict.items():
+#        if v == min_value:
+#            positions.append(k)
+#        if v < min_value:
+#            min_value = v
+#            positions = []  # output variable
+#            positions.append(k)
+#
+#    return positions
+def get_path(L, u, z, r=[]):
+    lz = L[z][1]
+    r.append(lz[-1])
+    if u in r:
+        return r
+    return get_path(L, u, lz[-1], r)
+
+
+def w(G, x, v):
+    try:
+        return G.es[G.get_eid(x, v)]["weight"]
+    except:
+        return float('inf')
+
+
+def Dijkstra(G, u, z):
+    # print(list(G.vs()))
+    #neis = G.neighbors(u)
+    # print(G.vs[neis]["name"])
+    #id = G.get_eid("Cowboy Bebop", "One Piece")
+    # print(G.es[id]['weight'])
+
+    L = {i: [float('inf'), []] for i in G.vs["name"]}
+    L[u] = [0, []]
     S = []
-    for i in range(len(A)):
-        L.append(float("inf"))
-    
-    o = A.index(u)
-    p = A.index(z)
 
-    L[o] = 0 
-    
     while z not in S:
-        print(S)
-        x = 0
-        for i in range(len(A)):
-            if A[i] not in S: 
-                if L[i] < L[x]:
-                    x = i
-        
-        S.append(A[x])
-        for i in range(len(A)):
-            if A[i] not in S:
-                if (A[x], A[i]) in C:
-                    m = C.index((A[x], A[i]))
-                    L[i] = min(L[i], L[x] + (B[m])[2])
+        L_S = {i: L[i][0] for i in L if i not in S}
+        x = min(L_S, key=L_S.get)
+        # for i in minimums(L):
+        #    if i not in S:
+        #        x = i
+        #        print(x)
+        #        # ime.sleep(0.5)
+        #        break
 
-                elif (A[i], A[x]) in C:
-                    m = C.index((A[i], A[x]))
-                    L[i] = min(L[i], L[x] + (B[m])[2])
-    
-    return S, L[p]
-
-A = ['A', 'B', 'C', 'D']
-C = [('A', 'B'), ('A', 'C'), ('A', 'D'),('B', 'D'), ('C', 'D')]
-B = [('A', 'B', 3), ('A', 'C', 4), ('A', 'D', 2), ('B', 'D', 5), ('C', 'D', 1)]
-
-P, p = DJ('C', 'B', A, B, C)
-print(P)
-print()
-print(p)
+        S = list(set().union(S, [x]))
+        # print(L)
+        for v in G.vs["name"]:
+            # print(v)
+            if v not in S:
+                if L[v][0] < L[x][0] + w(G, x, v):
+                    L[v] = [L[v][0], L[v][1]]
+                else:
+                    L[v][1].append(x)
+                    L[v] = [L[x][0] + w(G, x, v), L[v][1]]
+                # L[v] = min(L[v], L[x] + w(G, x, v))
+    path = get_path(L, u, z, [])
+    path = path[::-1]
+    path.append(z)
+    if L[z][0] == float('inf'):
+        path = []
+    print(f'{u} -- {z}: {path} w = {L[z][0]}')
+    return L
